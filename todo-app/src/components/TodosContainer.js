d71dc864f33879cd928926309e2368e96d324429
Sergey <sergeyripchanskiy@gmail.com>
2019-04-19 12:21:55 +0300

    use Redux store instead of react state

--- todo-app/src/components/TodosContainer.js
+++ todo-app/src/components/TodosContainer.js
@@ -1,44 +1,29 @@
 import React, { Component } from 'react'
 import axios from 'axios'
-import update from 'immutability-helper'
+import { connect } from 'react-redux'
+import { loadTodos, addTodo, toggleTodo, deleteTodo } from '../actions/actionCreators'
 
 class TodosContainer extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      todos: [],
-      inputValue: ''
-    }
-  }
 
   componentDidMount() {
     this.getTodos()
   }
 
-  handleChange = (e) => {
-    this.setState({inputValue: e.target.value});
-  };
-
   getTodos() {
     axios.get('/api/v1/todos')
       .then(response => {
-        this.setState({todos: response.data})
+        this.props.dispatch(loadTodos(response.data));
       })
       .catch(error => console.log(error))
   }
 
   createTodo = (e) => {
-    if (e.key === 'Enter') {
-      axios.post('/api/v1/todos', {todo: {title: e.target.value}})
+    if (e.key === 'Enter' && !(this.getTitle.value === '')) {
+      // axios.post('/api/v1/todos', {todo: {title: e.target.value}})
+      axios.post('/api/v1/todos', {todo: {title: this.getTitle.value}})
         .then(response => {
-          const todos = update(this.state.todos, {
-            $splice: [[0, 0, response.data]]
-          });
-          this.setState({
-            todos: todos,
-            inputValue: ''
-          })
+          this.props.dispatch(addTodo(response.data.id, response.data.title));
+          this.getTitle.value = '';
         })
         .catch(error => console.log(error))
     }
@@ -47,13 +32,7 @@ class TodosContainer extends Component {
   updateTodo = (e, id) => {
     axios.put(`/api/v1/todos/${id}`, {todo: {done: e.target.checked}})
       .then(response => {
-        const todoIndex = this.state.todos.findIndex(x => x.id === response.data.id)
-        const todos = update(this.state.todos, {
-          [todoIndex]: {$set: response.data}
-        });
-        this.setState({
-          todos: todos
-        })
+        this.props.dispatch(toggleTodo(id))
       })
       .catch(error => console.log(error))
   };
@@ -61,13 +40,7 @@ class TodosContainer extends Component {
   deleteTodo = (id) => {
     axios.delete(`/api/v1/todos/${id}`)
       .then(response => {
-        const todoIndex = this.state.todos.findIndex(x => x.id === id)
-        const todos = update(this.state.todos, {
-          $splice: [[todoIndex, 1]]
-        });
-        this.setState({
-          todos: todos
-        })
+        this.props.dispatch(deleteTodo(id))
       })
       .catch(error => console.log(error))
   };
@@ -79,11 +52,11 @@ class TodosContainer extends Component {
           <input className="taskInput" type="text"
                  placeholder="Add a task" maxLength="50"
                  onKeyPress={this.createTodo}
-                 value={this.state.inputValue} onChange={this.handleChange}/>
+                 ref={(input)=>this.getTitle = input}/>
         </div>
         <div className="listWrapper">
           <ul className="taskList">
-            {this.state.todos.map((todo) => {
+            {this.props.todos.map((todo) => {
               return(
                 <li className="task" todo={todo} key={todo.id}>
                   <input className="taskCheckbox" type="checkbox"
@@ -91,10 +64,9 @@ class TodosContainer extends Component {
                          onChange={(e) => this.updateTodo(e, todo.id)}/>
                   <label className="taskLabel">{todo.title}</label>
                   <span className="deleteTaskBtn"
-                        onClick={(e) => this.deleteTodo(todo.id)}>
+                        onClick={() => this.deleteTodo(todo.id)}>
                         x
                   </span>
-                  <span>{todo.done}</span>
                 </li>
               )
             })}
@@ -105,4 +77,10 @@ class TodosContainer extends Component {
   }
 }
 
-export default TodosContainer
+const mapStateToProps = (state) => {
+  return {
+    todos: state.todos
+  }
+};
+
+export default connect(mapStateToProps)(TodosContainer)
